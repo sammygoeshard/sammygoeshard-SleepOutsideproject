d55fdc58e840f2628b6b2ed9bbc8b5f2cc0d51d4
sammygoeshard <samueljaspe052@gmail.com>
2026-06-13 17:06:59 -0400

    patch 3

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,8 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         // add product listing and product details pages
         product_listing: resolve(__dirname, "src/product_listing/index.html"), 
-        product1: resolve(__dirname, "src/product_pages/cedar-ridge-rimrock-2.html"),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(__dirname, "src/product_pages/northface-alpine-3.html"),
-        product4: resolve(__dirname, "src/product_pages/northface-talus-4.html"),
+        // dynamic product link
+        product: resolve(__dirname, "src/product_pages/index.html"),
       },
       },
     },
